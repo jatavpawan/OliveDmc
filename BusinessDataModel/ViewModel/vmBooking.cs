126bf8a9aaac3f5d1e3453883b16db22f4647c9e
jatavpawan <jatav.pawan@gmail.com>
2020-09-28 00:31:13 +0530

    Added Repository and services

---
+++ BusinessDataModel/ViewModel/vmBooking.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessDataModel.ViewModel
+{
+    public partial class vmPredictCity
+    {
+        public string prefixText{ get; set; }
+        public int count { get; set; }
+  public string Listfor{ get; set; }
+      
+    }
+
+
+
+}
